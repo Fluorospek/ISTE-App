a2fc48bfbc8fefc92dbd8cc35768f0ade4e0ed50
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2022-06-18 17:09:40 +0530

    login page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   return runApp(
     MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        body: LoginPage(),
+      ),
     ),
   );
 }
@@ -16,8 +21,171 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  String email = 'Default';
+  String password = 'Default';
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.topCenter,
+            overflow: Overflow.visible,
+            children: [
+              Container(
+                height: 250.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/campus.png'),
+                    fit: BoxFit.fill,
+                    colorFilter: new ColorFilter.mode(
+                        Colors.black.withOpacity(0.65), BlendMode.dstATop),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: -10,
+                child: Container(
+                  height: 23.0,
+                  width: 700.0,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      stops: [0.1, 0.6],
+                      colors: [Colors.transparent, Colors.black],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(20, 60, 0, 0),
+                child: Positioned(
+                  height: 300,
+                  width: 500,
+                  child: Image(
+                    image: AssetImage('images/iste_logo.png'),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(color: Colors.black),
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    height: 30,
+                    child: Text(
+                      'LOGIN',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                    child: Text(
+                      'USERNAME',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: SizedBox(
+                      width: 320,
+                      height: 39,
+                      child: TextField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    padding: EdgeInsets.fromLTRB(60, 0, 0, 0),
+                    child: Text(
+                      'PASSWORD',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 17,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                    child: SizedBox(
+                      width: 320,
+                      height: 39,
+                      child: TextField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(13),
+                          ),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(0, 10, 170, 0),
+                    child: Text(
+                      'Forgot Password?',
+                      textAlign: TextAlign.right,
+                      style: TextStyle(
+                        color: Colors.lightBlueAccent,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: 320,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(shape: StadiumBorder()),
+                      child: Text('Submit'),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
